5f2c4ff282be633d68ac6b9f8417f1dac31f36d8
2024cetepalmaeralouie-boop <2024_cete_palmaeralouie@online.htcgsc.edu.ph>
2026-01-19 02:37:32 +0000

    awsavesave

--- hello.lua
+++ hello.lua
@@ -1,1 +1,6 @@
-print("Hello from GitHub!")
+local msg = Instance.new("Message")
+msg.Parent = game.Workspace
+msg.Text = "SUCCESS! The Executor loaded this from GitHub!"
+
+task.wait(3)
+msg:Destroy()
