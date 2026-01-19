582b290e85ea1e885ab4aff4393fc4a01cbd7e77
2024cetepalmaeralouie-boop <2024_cete_palmaeralouie@online.htcgsc.edu.ph>
2026-01-19 07:08:02 +0000

    sa

--- hello.lua
+++ hello.lua
@@ -8,7 +8,7 @@ local ReplicatedStorage = game:GetService("ReplicatedStorage")
 -- 0.05 = Very Fast (Might crash before gift sends)
 -- 0.50 = Medium (Good starting point)
 -- 1.00 = Slow (Might save data too fast)
-local CRASH_DELAY = 0.1 
+local CRASH_DELAY = 0.3
 
 -- ========================================================
 -- 2. THE TRAP (Silent Listener)
