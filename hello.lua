3a6df3c5a2a927d5ce2d16d492bb12368e1924c4
2024cetepalmaeralouie-boop <2024_cete_palmaeralouie@online.htcgsc.edu.ph>
2026-01-19 02:52:34 +0000

    s

--- hello.lua
+++ hello.lua
@@ -1,19 +1,19 @@
--- 1. Show a message immediately so we know the script loaded
+local player = game.Players.LocalPlayer
+local character = player.Character
+local rootPart = character:WaitForChild("HumanoidRootPart")
+
+-- 1. Create a floating red box above your head
+local box = Instance.new("Part")
+box.Size = Vector3.new(4, 4, 4)
+box.Position = rootPart.Position + Vector3.new(0, 10, 0) -- 10 studs up
+box.BrickColor = BrickColor.new("Really red")
+box.Material = Enum.Material.Neon
+box.Anchored = true
+box.Parent = game.Workspace
+
+-- 2. Tell you it worked
 local msg = Instance.new("Message")
-msg.Parent = workspace
-msg.Text = "Script Loaded! Searching for floor..."
-task.wait(2)
-
--- 2. Try to find the floor (Case insensitive search)
-local floor = workspace:FindFirstChild("Baseplate") or workspace:FindFirstChild("BasePlate") or workspace:FindFirstChild("Floor")
-
-if floor then
-    floor:Destroy()
-    msg.Text = "BOOM! Floor deleted."
-else
-    msg.Text = "Script worked, but I couldn't find a part named 'Baseplate'."
-end
-
--- 3. Cleanup
+msg.Parent = game.Workspace
+msg.Text = "LOOK UP! I created a block."
 task.wait(3)
 msg:Destroy()
