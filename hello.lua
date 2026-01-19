83c1275022a28b656e685bbdd551d2774d512ef9
2024cetepalmaeralouie-boop <2024_cete_palmaeralouie@online.htcgsc.edu.ph>
2026-01-19 02:45:43 +0000

    as

--- hello.lua
+++ hello.lua
@@ -1,13 +1,19 @@
-local floor = workspace:FindFirstChild("Baseplate") or workspace:FindFirstChild("BasePlate")
+-- 1. Show a message immediately so we know the script loaded
+local msg = Instance.new("Message")
+msg.Parent = workspace
+msg.Text = "Script Loaded! Searching for floor..."
+task.wait(2)
+
+-- 2. Try to find the floor (Case insensitive search)
+local floor = workspace:FindFirstChild("Baseplate") or workspace:FindFirstChild("BasePlate") or workspace:FindFirstChild("Floor")
 
 if floor then
     floor:Destroy()
-    
-    local msg = Instance.new("Message")
-    msg.Parent = workspace
-    msg.Text = "I DELETED THE FLOOR!"
-    task.wait(3)
-    msg:Destroy()
+    msg.Text = "BOOM! Floor deleted."
 else
-    print("Could not find floor to delete.")
+    msg.Text = "Script worked, but I couldn't find a part named 'Baseplate'."
 end
+
+-- 3. Cleanup
+task.wait(3)
+msg:Destroy()
