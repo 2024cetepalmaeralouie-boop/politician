c49c4628d98fe2179cb7f401a63115b68c5b99be
2024cetepalmaeralouie-boop <2024_cete_palmaeralouie@online.htcgsc.edu.ph>
2026-03-21 02:38:51 +0800

    Refactor button spy script for remote detection

--- hello.lua
+++ hello.lua
@@ -1,41 +1,23 @@
--- Save this to your GitHub as 'button_spy.lua'
-print("--- BUTTON SNIFFER ACTIVE ---")
+-- REMOTE SPY: Run this to see the HIDDEN data
+local mt = getrawmetatable(game)
+local old = mt.__namecall
+setreadonly(mt, false)
 
-local Player = game.Players.LocalPlayer
-local PlayerGui = Player:WaitForChild("PlayerGui")
-
--- Function to handle the click log
-local function connectButton(button)
-    button.MouseButton1Click:Connect(function()
+mt.__namecall = newcclosure(function(self, ...)
+    local method = getnamecallmethod()
+    local args = {...}
+    
+    -- We are looking for "FireServer" (the client talking to the server)
+    if method == "FireServer" or method == "InvokeServer" then
         print("-------------------------------")
-        print("BUTTON CLICKED: " .. button.Name)
-        print("PATH: " .. button:GetFullName())
-        
-        -- Check if the button has 'Attributes' (Common way to store data in 2026)
-        local attributes = button:GetAttributes()
-        if next(attributes) ~= nil then
-            print("ATTRIBUTES FOUND:")
-            for name, value in pairs(attributes) do
-                print("   -> " .. name .. ": " .. tostring(value))
-            end
-        else
-            print("No extra data attributes found on this button.")
+        print("📡 REMOTE DETECTED: " .. self.Name)
+        print("FROM PATH: " .. self:GetFullName())
+        for i, v in pairs(args) do
+            print("   Arg " .. i .. ": " .. tostring(v) .. " (" .. type(v) .. ")")
         end
-    end)
-end
-
--- Scan for all existing buttons (TextButtons and ImageButtons)
-for _, v in pairs(PlayerGui:GetDescendants()) do
-    if v:IsA("TextButton") or v:IsA("ImageButton") then
-        connectButton(v)
     end
-end
-
--- Also watch for NEW buttons that might be added later (like in menus)
-PlayerGui.DescendantAdded:Connect(function(v)
-    if v:IsA("TextButton") or v:IsA("ImageButton") then
-        connectButton(v)
-    end
+    
+    return old(self, ...)
 end)
 
-print("Monitoring all UI buttons... Click something to see data!")
+print("--- REMOTE SPY ACTIVE: Click 'Buy' now! ---")
